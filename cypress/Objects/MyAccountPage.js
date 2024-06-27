878163daabc5bdf40dbb13ae1d498142e9c9bd8d
ikhszy <ikhszy@gmail.com>
2024-06-28 01:57:13 +0700

    28062024_2

--- cypress/Objects/MyAccountPage.js
+++ cypress/Objects/MyAccountPage.js
@@ -23,6 +23,42 @@ class MyAccountPage {
     IconWishlist() {
         return cy.get('[data-original-title="My wish list"]').click()
     }
+
+    IconWishCount() {
+        return cy.get('[data-original-title="My wish list"]').find('.badge-success')
+    }
+
+    IconOrderHistory() {
+        return cy.get('[data-original-title="Order history"]').first().click()
+    }
+
+    IconOrderCount() {
+        return cy.get('[data-original-title="Order history"]').first().find('.badge-success')
+    }
+
+    IconTransHistory() {
+        return cy.get('[data-original-title="Transaction history"]').first().click()
+    }
+
+    IconTransCount() {
+        return cy.get('[data-original-title="Transaction history"]').first().find('.badge-success')
+    }
+
+    IconDownload() {
+        return cy.get('[data-original-title="Downloads"]').first().click()
+    }
+
+    IconDownloadCount() {
+        return cy.get('[data-original-title="Downloads"]').first().find('.badge-success')
+    }
+
+    IconNotification() {
+        return cy.get('[data-original-title="Notifications"]').click()
+    }
+
+    IconLogoff() {
+        return cy.get('[data-original-title="Logoff"]').click()
+    }
 }
 
 export default MyAccountPage
